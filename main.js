4f4781afc4e860578b6a292efa4d49890da2adb4
Ivan Scherbyna <crosell@yandex.ua>
2016-07-01 09:26:36 +0300

    fixed work js code

--- main.js
+++ main.js
@@ -1,37 +1,10 @@
-var obj = {
-	myString : 'str',
-	func : function() {
-		console.log( this.myString );
+jQuery(function ($) {
+	$( init );
+		function init() {
+
+         $("bootstrap_button").replaceWith(function(){
+          return '<button><span> '+$(this).text()+'</span></button>' ;
+        });
+
 	}
-};
-
-
-var prop = 'func';
-
-
-// console.log( obj.asd || 'name' );
-
-obj.odd = 'Nine 20';
-obj.myString = 'New String'
-
-// console.log( obj );
-
-var a = {prop: 1}, b = {prop: 2}, c = {prop: 3};
-
-
-a = b = c = {prop: 'allTheSame'};
-
-//console.log(a, b, c);
-
-var y = obj;
-
-//console.log( y.myString );
-
-// delete obj.func;
-
-// console.log( obj );
-obj.func();
-
-var MYAPP = {
-	
-};
+});
